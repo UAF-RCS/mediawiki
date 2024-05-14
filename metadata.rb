d09a2f192f67936b9a07becd47e2ca58fdb1765f
Jeremiah Dabney <jdabney@gmail.com>
2024-05-13 18:18:09 -0800

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@rcs.alaska.edu'
 license          'Apache 2.0'
 description      'Installs/Configures mediawiki'
 long_description 'Installs/Configures mediawiki'
-version          '4.0.12'
+version          '4.0.13'
 
 chef_version '> 17.0'
 
